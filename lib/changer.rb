61a0d7fb748a02f74357d3ef444d46326500bfbe
Benjamin Hand <gneeshot@gmail.com>
2015-06-29 09:47:47 -0500

    all tests passing

--- lib/changer.rb
+++ lib/changer.rb
@@ -1,5 +1,29 @@
 class Changer
   def self.run(amt)
-
+    coins = []
+    [25,10,5,1].each do |coin|
+      times = amt / coin
+      coins += [coin] * times
+      amt -= coin * times
+    end
+      return coins
   end
 end
+# Original Train of Thought. Ben Hand - Gracie Xia
+    # if amt % 25 == 0
+    #   times = amt / 25
+    #   coins += [25] * times
+    #   return coins
+    # elsif amt % 10 == 0
+    #   times= amt / 10
+    #   coins += [10] * times
+    #   return coins
+    # elsif amt % 5 == 0
+    #   times = amt / 5
+    #   coins += [5] * times
+    #   return coins
+    # elsif amt % 1 == 0
+    #   times = amt / 1
+    #   coins += [1] * times
+    #   return coins
+    # end
--- spec/changer_test.rb
+++ spec/changer_test.rb
@@ -12,4 +12,47 @@ class ChangerTest < MiniTest::Test
     assert_respond_to(Changer, :run)
   end
 
+  def test_run_returns_quarter
+    assert_equal([25], Changer.run(25))
+  end
+
+  def test_run_returns_dime
+    assert_equal([10], Changer.run(10))
+  end
+
+  def test_run_returns_nickel
+    assert_equal([5], Changer.run(5))
+  end
+
+  def test_run_return_penny
+    assert_equal([1], Changer.run(1))
+  end
+
+  def test_run_75_returns_3_quarters
+    assert_equal([25, 25, 25], Changer.run(75))
+  end
+
+  def test_run_20_return_2_dimes
+    assert_equal([10,10],Changer.run(20))
+  end
+
+  def test_run_5_returns_1_nickel
+    assert_equal([5], Changer.run(5))
+  end
+
+  def test_run_4_return_4_pennies
+    assert_equal([1,1,1,1],Changer.run(4))
+  end
+
+  def test_run_27_returns_coins
+    assert_equal([25, 1, 1], Changer.run(27))
+  end
+
+  def test_run_66_return_coins
+    assert_equal([25,25,10,5,1], Changer.run(66))
+  end
+
+  def test_run_93_return_coins
+    assert_equal([25,25,25,10,5,1,1,1], Changer.run(93))
+  end
 end
